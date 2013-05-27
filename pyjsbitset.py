d56a07b9f249110b21b22c9671375a8cec85ee88
James Garnon <jg.gatc@gmail.com>
2013-05-27 12:25:18 -0400

    minor syntax change

--- pyjsbitset.py
+++ pyjsbitset.py
@@ -13,7 +13,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#PyjsBitset version 1.0
+#PyjsBitset version 0.5
 #Project Site: http://gatc.ca
 
 import math
